55e69a644001ebb7ed5dd9a946a1e20fb17762ab
michael <michail194@mail.ru>
2023-04-06 17:26:50 +0300

    step 7 done

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,32 @@
-import { test, expect } from '@jest/globals';
-import result from '../__fixtures__/result.js';
+import { test, expect, describe } from '@jest/globals';
+import resultStylish from '../__fixtures__/resultStylish.js';
+import resultPlain from '../__fixtures__/resultPlain.js';
 import showDiff from '../src/index.js';
 
-test('json', () => {
-  expect(showDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(result);
+describe('stylish', () => {
+  test('json', () => {
+    expect(showDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(resultStylish);
+  });
+
+  test('yaml', () => {
+    expect(showDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml')).toBe(resultStylish);
+  });
+
+  test('yml', () => {
+    expect(showDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toBe(resultStylish);
+  });
 });
 
-test('yaml', () => {
-  expect(showDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml')).toBe(result);
-});
+describe('plain', () => {
+  test('json', () => {
+    expect(showDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain')).toBe(resultPlain);
+  });
 
-test('yml', () => {
-  expect(showDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toBe(result);
+  test('yaml', () => {
+    expect(showDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml', 'plain')).toBe(resultPlain);
+  });
+
+  test('yml', () => {
+    expect(showDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'plain')).toBe(resultPlain);
+  });
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import makeStylish from './stylish.js';
+import makePlain from './plain.js';
 
 export default function formatter(tree, format) {
   switch (format) {
     case 'stylish':
       return makeStylish(tree);
+    case 'plain':
+      return makePlain(tree);
     default:
       throw new Error('vvedite format');
   }
