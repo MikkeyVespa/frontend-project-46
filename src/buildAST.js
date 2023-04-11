1ec934beb90b2941458d037cae02be44b9c7de2d
michael <michail194@mail.ru>
2023-04-11 13:42:32 +0300

    fix2 func buildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,21 +1,22 @@
 import _ from 'lodash';
 
 const buildAST = (data1, data2) => {
-  const keys = Object.keys({ ...data1, ...data2 }).sort();
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
 
-  return keys.map((key) => {
-    if (_.isEqual(data1[key], data2[key])) {
-      return { type: 'unchanged', key, value: data1[key] };
-    } if (!_.has(data1, key)) {
+  const children = keys.map((key) => {
+    if (!_.has(data1, key)) {
       return { type: 'added', key, value: data2[key] };
     } if (!_.has(data2, key)) {
       return { type: 'removed', key, value: data1[key] };
     } if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return { type: 'nested', key, children: buildAST(data1[key], data2[key]) };
+    } if (data1[key] === data2[key]) {
+      return { type: 'unchanged', key, value: data1[key] };
     } return {
       type: 'changed', key, value: data1[key], value2: data2[key],
     };
   });
+  return children;
 };
 
 const getDifferenceTree = (data1, data2) => ({
