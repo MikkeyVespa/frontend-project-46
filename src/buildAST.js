0b86a8d36a9b81d2559235070aa72e6a673dfcbc
michael <michail194@mail.ru>
2023-04-11 16:49:16 +0300

    change func BuildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -5,35 +5,16 @@ const buildAST = (data1, data2) => {
 
   const children = keys.map((key) => {
     if (!_.has(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: data2[key],
-      };
+      return { type: 'added', key, value: data2[key] };
     } if (!_.has(data2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value: data1[key],
-      };
+      return { type: 'removed', key, value: data1[key] };
     } if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return {
-        type: 'nested',
-        key,
-        children: buildAST(data1[key], data2[key]),
-      };
+      return { type: 'nested', key, children: buildAST(data1[key], data2[key]) };
     }
     if (data1[key] === data2[key]) {
-      return {
-        type: 'unchanged',
-        key,
-        value: data1[key],
-      };
+      return { type: 'unchanged', key, value: data1[key] };
     } return {
-      type: 'changed',
-      key,
-      value: data1[key],
-      value2: data2[key],
+      type: 'changed', key, value: data1[key], value2: data2[key],
     };
   });
   return children;
