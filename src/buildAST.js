6fa64db5b15c84c74a5dc0d6647af09c728b6e7c
michael <michail194@mail.ru>
2023-04-11 14:23:23 +0300

    change buildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const buildAST = (data1, data2) => {
-  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   const children = keys.map((key) => {
     if (!_.has(data1, key)) {
