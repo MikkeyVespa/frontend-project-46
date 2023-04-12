e9e7f9d75127366aebcca14014b1ee715b5ce0da
michael <michail194@mail.ru>
2023-04-12 13:56:15 +0300

    return buildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,32 +1,42 @@
 import _ from 'lodash';
 
 const buildAST = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
 
-  return keys.map((key) => {
+  const children = keys.map((key) => {
     if (!_.has(data1, key)) {
-      return { type: 'added', key, value: data2[key] };
+      return {
+        type: 'added',
+        key,
+        value: data2[key],
+      };
+    } if (!_.has(data2, key)) {
+      return {
+        type: 'removed',
+        key,
+        value: data1[key],
+      };
+    } if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        type: 'nested',
+        key,
+        children: buildAST(data1[key], data2[key]),
+      };
     }
-    if (!_.has(data2, key)) {
-      return { type: 'removed', key, value: data1[key] };
-    }
-    const value1 = data1[key];
-    const value2 = data2[key];
-
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { type: 'nested', key, children: buildAST(value1, value2) };
-    }
-
-    if (_.isEqual(value1, value2)) {
-      return { type: 'unchanged', key, value: value1 };
-    }
-
-    return {
-      type: 'changed', key, value: value1, value2,
+    if (data1[key] === data2[key]) {
+      return {
+        type: 'unchanged',
+        key,
+        value: data1[key],
+      };
+    } return {
+      type: 'changed',
+      key,
+      value: data1[key],
+      value2: data2[key],
     };
   });
+  return children;
 };
 
 const getDifferenceTree = (data1, data2) => ({
